214b9f327a1eec4de6fa09cafc4a766e4b2c6d47
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-10-09 16:00:33 -0700

    Bump version: 0.0.9 → 0.0.10

--- neuroglancerjsonserver/app_blueprint.py
+++ neuroglancerjsonserver/app_blueprint.py
@@ -7,7 +7,7 @@ import datetime
 from neuroglancerjsonserver import database
 
 bp = Blueprint('neuroglancerjsonserver', __name__, url_prefix="/ngl_state")
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 # -------------------------------
 # ------ Access control and index
 # -------------------------------
