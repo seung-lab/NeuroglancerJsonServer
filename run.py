e69ee89e4d789f6a6c0bff8c427c1afddf9fbb16
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-10-09 15:44:30 -0700

    mader server compatible with dockerization

---
+++ run.py
@@ -0,0 +1,17 @@
+from werkzeug.serving import WSGIRequestHandler
+from neuroglancerjsonserver import create_app
+import os
+
+HOME = os.path.expanduser("~")
+
+application = create_app()
+
+if __name__ == '__main__':
+
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+
+    application.run(host='0.0.0.0',
+                    port=4001,
+                    debug=True,
+                    threaded=True,
+                    ssl_context='adhoc')
