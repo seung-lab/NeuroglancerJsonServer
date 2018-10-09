b6325dafd8cb608f89384eb294c3a6a7ad50dcbb
Forrest Collman <forrest.collman@gmail.com>
2018-10-09 15:21:59 -0700

    changing version to sync with bumpversion

--- neuroglancerjsonserver/app_blueprint.py
+++ neuroglancerjsonserver/app_blueprint.py
@@ -7,7 +7,7 @@ import datetime
 from neuroglancerjsonserver import database
 
 bp = Blueprint('neuroglancerjsonserver', __name__, url_prefix="/ngl_state")
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 # -------------------------------
 # ------ Access control and index
 # -------------------------------
