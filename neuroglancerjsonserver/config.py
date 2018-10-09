150403a4b935089f68b84f17e83fdecb589564a0
Forrest Collman <forrest.collman@gmail.com>
2018-10-09 16:16:17 -0700

    adding 'CORS_HEADERS' 'Content-Type' to config

--- neuroglancerjsonserver/config.py
+++ neuroglancerjsonserver/config.py
@@ -6,6 +6,6 @@ class BaseConfig(object):
     DEBUG = False
     TESTING = False
     HOME = os.path.expanduser("~")
-
+    CORS_HEADERS = 'Content-Type'
     SECRET_KEY = '1d94e52c-1c89-4515-b87a-f48cf3cb7f0b'
     LOGGING_LEVEL = logging.DEBUG
