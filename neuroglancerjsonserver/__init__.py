77d7cc71b7ed5504769f5f0b62d8ce5fa3d35e3d
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2019-01-14 17:09:06 -0800

    Bump version: 0.2.2 → 0.2.3

--- neuroglancerjsonserver/__init__.py
+++ neuroglancerjsonserver/__init__.py
@@ -6,7 +6,7 @@ from . import config
 from .app_blueprint import bp
 import logging
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 
 def create_app(test_config=None):
--- neuroglancerjsonserver/app_blueprint.py
+++ neuroglancerjsonserver/app_blueprint.py
@@ -7,7 +7,7 @@ import datetime
 from neuroglancerjsonserver import database
 
 bp = Blueprint('neuroglancerjsonserver', __name__, url_prefix="/nglstate")
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 # -------------------------------
 # ------ Access control and index
 # -------------------------------
