7af6e2ababa3dcfd0afb6822fae87a81a56f552f
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2019-02-01 15:05:42 -0500

    minor formatting

--- neuroglancerjsonserver/migration.py
+++ neuroglancerjsonserver/migration.py
@@ -1,22 +1,23 @@
 import json
 
 
+def convert_layer(layer_obj):
+    if layer_obj["type"] == 'segmentation':
+        if layer_obj['source'].startswith("precomputed://gs://neuroglancer/nkem/pinky100_v0/ws/lost_no-random/bbox1_0"):
+            layer_obj['source'] = "graphene://https://www.dynamicannotationframework.com/segmentation/1.0/pinky100_sv16"
+            if 'chunkedGraph' in layer_obj:
+                del layer_obj['chunkedGraph']
+
+
 def convert_precomputed_to_graphene_v1(json_data):
     j = json.loads(json_data)
     layers = j["layers"]
 
-    def convertLayer(layerObj):
-       if layerObj["type"] == 'segmentation':
-            if layerObj['source'].startswith("precomputed://gs://neuroglancer/nkem/pinky100_v0/ws/lost_no-random/bbox1_0"):
-                layerObj['source'] = "graphene://https://www.dynamicannotationframework.com/segmentation/1.0/pinky100_sv16"
-                if 'chunkedGraph' in layerObj:
-                    del layerObj['chunkedGraph'] 
-
     if isinstance(layers, list):
         for layer in layers:
-            convertLayer(layer)
+            convert_layer(layer)
     else:
         for l in layers.keys():
-            convertLayer(layers[l])
+            convert_layer(layers[l])
 
     return json.dumps(j)
