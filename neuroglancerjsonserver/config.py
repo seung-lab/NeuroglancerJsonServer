b61531a81e5c8784b30eb5e63e45ae926c0ac3c1
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2019-10-18 09:53:00 -0400

    added config for redis

--- neuroglancerjsonserver/config.py
+++ neuroglancerjsonserver/config.py
@@ -10,3 +10,12 @@ class BaseConfig(object):
     SECRET_KEY = '1d94e52c-1c89-4515-b87a-f48cf3cb7f0b'
     LOGGING_LEVEL = logging.DEBUG
     JSON_SORT_KEYS = False
+
+
+class DeploymentWithRedisConfig(BaseConfig):
+    """Deployment configuration with Redis."""
+    USE_REDIS_JOBS = True
+    REDIS_HOST = os.environ.get('REDIS_SERVICE_HOST')
+    REDIS_PORT = os.environ.get('REDIS_SERVICE_PORT')
+    REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+    REDIS_URL = f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0'
