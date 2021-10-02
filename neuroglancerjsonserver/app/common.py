9d2e9824d74022f3bee5bfb41b319b69fb8031d9
Akhilesh Halageri <akhileshhalageri@gmail.com>
2021-10-02 23:37:37 +0000

    feat: endpoint for user states

--- neuroglancerjsonserver/app/common.py
+++ neuroglancerjsonserver/app/common.py
@@ -13,13 +13,14 @@ __api_versions__ = [0]
 # ------ Access control and index
 # -------------------------------
 
+
 def index():
     return f"NeuroglancerJsonServer - v{__version__}"
 
 
 def home():
     resp = make_response()
-    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers["Access-Control-Allow-Origin"] = "*"
     acah = "Origin, X-Requested-With, Content-Type, Accept"
     resp.headers["Access-Control-Allow-Headers"] = acah
     resp.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
@@ -31,6 +32,7 @@ def home():
 # ------ Measurements and Logging
 # -------------------------------
 
+
 def before_request():
     g.request_start_time = time.time()
 
@@ -39,10 +41,16 @@ def after_request(response):
     dt = (time.time() - g.request_start_time) * 1000
 
     url_split = request.url.split("/")
-    current_app.logger.info("%s - %s - %s - %s - %f.3" %
-                            (request.path.split("/")[-1], "1",
-                             "".join([url_split[-2], "/", url_split[-1]]),
-                             str(request.data), dt))
+    current_app.logger.info(
+        "%s - %s - %s - %s - %f.3"
+        % (
+            request.path.split("/")[-1],
+            "1",
+            "".join([url_split[-2], "/", url_split[-1]]),
+            str(request.data),
+            dt,
+        )
+    )
 
     print("Response time: %.3fms" % (dt))
     return response
@@ -52,11 +60,16 @@ def internal_server_error(error):
     dt = (time.time() - g.request_start_time) * 1000
 
     url_split = request.url.split("/")
-    current_app.logger.error("%s - %s - %s - %s - %f.3" %
-                             (request.path.split("/")[-1],
-                              "Server Error: " + error,
-                              "".join([url_split[-2], "/", url_split[-1]]),
-                              str(request.data), dt))
+    current_app.logger.error(
+        "%s - %s - %s - %s - %f.3"
+        % (
+            request.path.split("/")[-1],
+            "Server Error: " + error,
+            "".join([url_split[-2], "/", url_split[-1]]),
+            str(request.data),
+            dt,
+        )
+    )
     return 500
 
 
@@ -64,52 +77,58 @@ def unhandled_exception(e):
     dt = (time.time() - g.request_start_time) * 1000
 
     url_split = request.url.split("/")
-    current_app.logger.error("%s - %s - %s - %s - %f.3" %
-                             (request.path.split("/")[-1],
-                              "Exception: " + str(e),
-                              "".join([url_split[-2], "/", url_split[-1]]),
-                              str(request.data), dt))
+    current_app.logger.error(
+        "%s - %s - %s - %s - %f.3"
+        % (
+            request.path.split("/")[-1],
+            "Exception: " + str(e),
+            "".join([url_split[-2], "/", url_split[-1]]),
+            str(request.data),
+            dt,
+        )
+    )
     return 500
+
 
 # -------------------
 # ------ Applications
 # -------------------
 
+
 def get_json(json_id):
+    user_id = str(g.auth_user["id"])
     db = app_utils.get_json_db()
-
-    json_data = db.get_json(int(json_id), decompress=True)
-
+    json_data = db.get_json(int(json_id), user_id, decompress=True)
     return jsonify(json.loads(json_data))
+
+
+def get_user_jsons(start_time, end_time):
+    user_id = str(g.auth_user["id"])
+    db = app_utils.get_json_db()
+    json_data = db.get_user_jsons(user_id, start_time, end_time)
+    return jsonify(json_data)
 
 
 def get_raw_json(json_id):
     db = app_utils.get_json_db()
-
     json_data = db.get_json(int(json_id), decompress=False)
-
     return json_data
 
 
 def add_json(json_id=None, timestamp=None):
     user_id = str(g.auth_user["id"])
     db = app_utils.get_json_db()
-
     # Verify that data is json
     try:
         _ = json.loads(request.data)
     except ValueError:
         raise ValueError
-
-    json_id = db.add_json(request.data, user_id=user_id, 
-                          json_id=json_id, date=timestamp)
+    json_id = db.add_json(request.data, user_id, json_id, date=timestamp)
     url_base = request.url.strip("/").rsplit("/", 1)[0]
-
     return jsonify("{}/{}".format(url_base, json_id))
 
 
 def table_info():
     user_id = str(g.auth_user["id"])
     db = app_utils.get_json_db()
-
     return f"User: {user_id} - project {db.project_id} - table {db.namespace}"
--- neuroglancerjsonserver/backend/database.py
+++ neuroglancerjsonserver/backend/database.py
@@ -1,6 +1,6 @@
 import os
 import zlib
-import datetime
+from datetime import datetime
 from google.cloud import datastore
 from cloudfiles import CloudFiles
 
@@ -19,7 +19,6 @@ class JsonDataBase(object):
         else:
             assert project_id is not None
             self._client = datastore.Client(project=project_id, credentials=credentials)
-
         self._namespace = table_name
 
     @property
@@ -42,7 +41,15 @@ class JsonDataBase(object):
     def json_column(self):
         return "v1"
 
-    def add_json(self, json_data, user_id, json_id=None, date=None):
+    @property
+    def bucket_name(self):
+        kind = "nsl_json_bucket_name"
+        key_name = "gcs_bucket_name"
+        key = self.client.key(kind, key_name, namespace=self.namespace)
+        entity = self.client.get(key)
+        return entity["value"]
+
+    def add_json(self, json_data: dict, user_id: str, json_id: int = None, date=None):
         if json_id is None:
             key = self.client.key(self.kind, namespace=self.namespace)
         else:
@@ -57,34 +64,25 @@ class JsonDataBase(object):
             raise Exception(f"[{self.namespace}][{key}][{json_id}] ID already exists.")
 
         entity = datastore.Entity(key, exclude_from_indexes=(self.json_column,))
-
         data = zlib.compress(json_data)
-        entity[self.json_column] = key.id
-        self._add_data_to_bucket(str(key.id), data)
+        self._add_data_to_bucket(str(key.id), user_id, data)
         entity["access_counter"] = int(1)
         entity["user_id"] = user_id
-
-        now = datetime.datetime.utcnow()
+        now = datetime.utcnow()
         if date is None:
             date = now
 
         entity["date"] = date
         entity["date_last"] = now
-
         self.client.put(entity)
-
         return entity.key.id
 
-    def get_json(self, json_id, decompress=True):
+    def get_json(self, json_id: int, user_id: str, decompress: bool = True):
         key = self.client.key(self.kind, json_id, namespace=self.namespace)
-
         entity = self.client.get(key)
-
-        assert self.json_column in entity.keys()
-
         json_data = entity.get(self.json_column)
         if json_data is None:
-            json_data = self._get_data_from_bucket(str(json_id))
+            json_data = self._get_data_from_bucket(str(json_id), user_id)
 
         if decompress:
             json_data = zlib.decompress(json_data)
@@ -94,22 +92,33 @@ class JsonDataBase(object):
         else:
             entity["access_counter"] = int(2)
 
-        entity["date_last"] = datetime.datetime.utcnow()
-
+        entity["date_last"] = datetime.utcnow()
         self.client.put(entity)
-
         return json_data
 
-    def _get_data_from_bucket(self, state_id: str):
-        bucket_path = os.getenv("NGL_STATE_BUCKET")
-        assert bucket_path is not None, "Bucket must be specified."
+    def get_user_jsons(
+        self, user_id: str, start_date: datetime, end_date: datetime
+    ) -> list:
+        from json import loads
 
-        cf = CloudFiles(bucket_path)
+        query = self.client.query(kind=self.kind)
+        query = query.add_filter("user_id", "=", user_id)
+        query = query.add_filter("date", ">=", start_date)
+        query = query.add_filter("date", "<=", end_date)
+        query.keys_only()
+        json_ids = list(query.fetch())
+        result = []
+        for _id in json_ids:
+            state = loads(zlib.decompress(self._get_data_from_bucket(_id, user_id)))
+            result.append(state)
+        return result
+
+    def _get_data_from_bucket(self, state_id: str, user_id: str) -> bytes:
+        path = f"{self.bucket_name}/{user_id}"
+        cf = CloudFiles(path)
         return cf.get(state_id)
 
-    def _add_data_to_bucket(self, state_id: str, data):
-        bucket_path = os.getenv("NGL_STATE_BUCKET")
-        assert bucket_path is not None, "Bucket must be specified."
-
-        cf = CloudFiles(bucket_path)
+    def _add_data_to_bucket(self, state_id: str, user_id: str, data: bytes) -> None:
+        path = f"{self.bucket_name}/{user_id}"
+        cf = CloudFiles(path)
         cf.put(state_id, data)
